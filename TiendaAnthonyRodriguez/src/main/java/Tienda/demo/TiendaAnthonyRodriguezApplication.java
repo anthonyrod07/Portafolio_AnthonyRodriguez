27290671fef9250c458742c27b92e625c23385fb
anthonyrod07 <144976528+anthonyrod07@users.noreply.github.com>
2024-09-25 20:05:15 -0600

    Creacion de Proyecto
    Se creo proyecto desde 0

---
+++ TiendaAnthonyRodriguez/src/main/java/Tienda/demo/TiendaAnthonyRodriguezApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAnthonyRodriguezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAnthonyRodriguezApplication.class, args);
+	}
+
+}
